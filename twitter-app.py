42e6d9bd95d7f991c8b717e25e6fb959a3fb3dca
JqKOU <62406178+JqKOU@users.noreply.github.com>
2021-01-21 03:30:09 -0500

    Update twitter-app.py

--- twitter-app.py
+++ twitter-app.py
@@ -7,7 +7,11 @@ from sklearn.naive_bayes import MultinomialNB
 st.write("""
 # Twitter User Type Prediction App
 
-This app predicts whether a Twitter user is a **Programmer** or a **Gamer**.
+This app predicts whether a Twitter user is a **Programmer** or a **Gamer** based on his/her user description.
+
+If a user posts tweets with keywords python, java, and c++, this user is more likely to be a programmer.
+
+If a user posts tweets with keywords PokemonGo, AnimalCrossing, and ACNH, this user is more likely to be a gamer.
 
 Model is based on data collected from Twitter with keywords of programmer (python, java, c++) and gamer (PokemonGo, AnimalCrossing, ACNH)
 """)
@@ -17,8 +21,8 @@ load_mnb = pickle.load(open('mnb_model.pkl', 'rb'))
 
 st.subheader('Input your Twitter Description')
 
-List = st.text_input('Input your description here:') 
-#List = list(description.split())
+description = st.text_input('Input your description here:') 
+List = list(description.split())
 
 # Apply model to make predictions
 prediction = load_mnb.predict(List)[0]
