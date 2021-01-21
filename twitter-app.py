75b7f587470489b21e33c99b55ff8afa8e50e708
JqKOU <62406178+JqKOU@users.noreply.github.com>
2021-01-21 04:35:29 -0500

    Update twitter-app.py

--- twitter-app.py
+++ twitter-app.py
@@ -5,31 +5,6 @@ import pickle
 from sklearn.naive_bayes import MultinomialNB
 import nltk
 
-import re
-from nltk import word_tokenize
-#from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-word_lemmatizer = WordNetLemmatizer()
-
-def preprocess(row):
-
-    # lowercasing, tokenization, and keep only alphabetical tokens
-    tokens = [word for word in word_tokenize(row.lower())\
-              if word.isalpha()]
-    # filtering out tokens that are not all alphabetical
-    tokens = [word for word in re.findall(r'[A-Za-z]+', ' '.join(tokens))]
-    # remove all stopwords
-    #no_stop = [word for word in tokens\
-              # if word not in stopwords.words('english')]
-    # lematizing all tokens
-    lemmatized = [word_lemmatizer.lemmatize(word) for word in tokens]
-    # convert tokens back to a sentense as the input for CountVectorizer later
-    processed = ' '.join(lemmatized)
-
-    # return the clean sentense
-    return processed
-
-
 st.write("""
 # Twitter User Type Prediction App
 
@@ -41,7 +16,7 @@ load_mnb = pickle.load(open('mnb_model.pkl', 'rb'))
 
 st.subheader("Input your Twitter Description")
 description = st.text_input("input your description here")
-Token = preprocess(description)
+Token = description.lower() 
 
 # Apply model to make predictions
 prediction = load_mnb.predict([Token])
